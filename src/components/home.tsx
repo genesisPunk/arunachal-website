37ebfa8e4468c99957f3b8b30f29805199e5b05a
PowerAlgo <cryptowithsahil@gmail.com>
2025-08-17 21:33:43 +0530

    updated the facebook page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -680,7 +680,7 @@ const Home = () => {
                   {/* Content */}
                   <div className="mb-3">
                     <iframe
-                      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100089994003445&tabs=timeline&width=224&height=315&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100089994003445&tabs=timeline&width=500&height=315&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
                       width="100%"
                       height="315"
                       style={{ border: "none", overflow: "hidden" }}
